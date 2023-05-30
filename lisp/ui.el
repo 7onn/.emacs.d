4b6c50629394df36b5bc30d2e2c322332cadd683
Tom M G <devbytom@gmail.com>
2023-05-31 00:03:16 +0200

    Set transparency 80%

--- lisp/ui.el
+++ lisp/ui.el
@@ -32,5 +32,5 @@
  '(default ((t (:family "Fira Code" :height 120)))))
 
 ;; transparency
-(set-frame-parameter (selected-frame) 'alpha '(90 90))
-(add-to-list 'default-frame-alist '(alpha 90 90))
+(set-frame-parameter (selected-frame) 'alpha '(80 80))
+(add-to-list 'default-frame-alist '(alpha 80 80))
